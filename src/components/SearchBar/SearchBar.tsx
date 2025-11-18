dab754f7ae4e308c787d1fdb2291c14a3dda45d2
Roman <neirity@gmail.com>
2025-11-18 22:13:17 +0200

    Initial commit

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,56 @@
+import styles from './SearchBar.module.css'
+import toast, { Toaster } from 'react-hot-toast';
+
+interface SearchBarProps {
+  onSubmit: (value: string) => void;
+}
+
+function SearchBar({ onSubmit }: SearchBarProps) {
+
+        const handleSubmit = (formData: FormData) => {
+        
+            const request = formData.get('query') as string;
+            
+    if (!request.trim()) {
+      toast.error('Please enter your search query.');
+      return;
+    }
+            onSubmit(request);
+            
+            console.log(request);
+            
+       
+    };
+   
+ 
+
+    return (
+    <header className={styles.header}>
+        <div className={styles.container}>
+            <a
+                className={styles.link}
+                href="https://www.themoviedb.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Powered by TMDB
+            </a>
+            <form className={styles.form} action={handleSubmit}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    name="query"
+                    autoComplete="off"
+                    placeholder="Search movies..."
+                    autoFocus
+                />
+                <button className={styles.button} type="submit">
+                    Search
+                </button>
+                </form>
+                <Toaster position="top-center" />
+        </div>
+    </header>)
+}
+
+export default SearchBar
